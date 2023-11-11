8bb2e76121a578f095204ef196b8446c039e3c26
Ridho Aji <ridhoaji921@gmail.com>
2023-11-11 17:51:17 +0700

    LA pert4

--- pertemuan 4/LA.cpp
+++ pertemuan 4/LA.cpp
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <conio.h>
-#include <iostream>
 #define max 5
 
-typedef struct{
+typedef struct {
     int top;
-    int data[max+1];
-}stack;
+    int data[max + 1];
+} stack;
 
 stack tumpukan;
 void createEmpty();
@@ -15,85 +14,86 @@ int IsFull();
 void push(int x);
 void pop();
 
-main(){
-    int lagi;
-    int input;
-    int pilih;
-    createEmpty();
-    pilih = 0;
-    while (pilih != 5){
-        puts("______________________________________");
-        printf("               MENU UTAMA\n");
-        puts("______________________________________");
-        puts("1: Cek kondisi Stack");
-        puts("2: Tambah data (Pust)");
-        puts("3: Keluarkan isi stack (pop)");
-        puts("4: Kosongkan Stack");
-        puts("5: Keluar");
-        printf("Pilihan Anda: ");
-        std::cin >> pilih;
-        switch(pilih){
-            case 1:
-            if (IsEmpty() == 1)
-                puts("Stack masih kosong");
-            else if ((IsEmpty() == 0) && (IsFull() == 0))
-                puts("Stack sudah terisi (belum penuh)");
-            else 
-                puts("Stack sudah penuh");
-            break;
+main () {
+	int lagi;
+	int input;
+	int pilih;
+	createEmpty();
+	pilih = 0;
+	while (pilih != 5){
+		puts("_________________________________");
+		printf("                   MENU UTAMA\n");
+		//printf dan puts memiliki fungsi yang sama
+		puts("1: Cek kondisi Stack");
+		puts("2: Tambah data (Pust)");
+		puts("3: Keluarkan isi stack (Pop)");
+		puts("4: Kosongkan Stack");
+		puts("5: Keluar");
+		printf("Pilihan Anda: ");
+		scanf("%d",&pilih);
+		switch(pilih){
+			case 1:
+                if (IsEmpty() == 1)
+                    puts("Stack masih kosong");
+                else if ((IsEmpty() == 0) && (IsFull() == 0))
+                    puts("Stack sudah terisi (belum penuh)");
+                else
+                    puts("Stack sudah penuh");
+                break;
             case 2:
                 if (IsFull() == 1)
-                    puts("Stack penuh.");
-                else
-                {
-                    printf("Masukkan Data: ");
-                    scanf("%d",&input);
+                    puts("Stack Penuh.");
+                else {
+                    printf("Masukkan data: ");
+                    scanf("%d", &input);
                     push(input);
-                    printf("Top baru: %d\n",tumpukan.data[tumpukan.top]);
-                    printf("IsFull: %d\n",IsFull());
-                    printf("IsEmpty: %d\n",IsEmpty());
+                    printf("Top baru: %d\n", tumpukan.data[tumpukan.top]);
+                    printf("IsFull: %d\n", IsFull());
+                    printf("IsEmpty: %d\n", IsEmpty());
                 }
-            break;
+                break;
             case 3:
-                printf("Top yang dikeluarkan bernilai: %d\n",tumpukan.data[tumpukan.top]);
-                pop();
-            break;
+                if (IsEmpty() == 1)
+                    puts("Stack masih kosong");
+                else {
+                    printf("Top yang dikeluarkan bernilai: %d\n", tumpukan.data[tumpukan.top]);
+                    pop();
+                }
+                break;
             case 4:
-                while (IsEmpty() == 0)
-            {
-                printf("Tumpukan yang dikeluarkan bernilaiL %d\n",tumpukan.data[tumpukan.top]);
-                pop();
-            }
-            puts("Stack sudah kosong");
-        break;
-        case 5:
-            puts("=====Terimakasih=====");
-        break;
-        }
-    }
-    return 0;
+                while (IsEmpty() == 0) {
+                    printf("Top yang dikeluarkan bernilai: %d\n", tumpukan.data[tumpukan.top]);
+                    pop();
+                }
+                puts("Stack sudah kosong");
+                break;
+            case 5:
+                puts("---Terimakasih---");
+                break;				
+		}
+	}
 }
-
-void createEmpty(){
+void createEmpty() {
     tumpukan.top = 0;
 }
 
-void push(int x){
+void push(int x) {
     tumpukan.top = tumpukan.top + 1;
     tumpukan.data[tumpukan.top] = x;
 }
 
-void pop(){
+void pop() {
     tumpukan.top = tumpukan.top - 1;
 }
-int IsEmpty(){
+
+int IsEmpty() {
     if (tumpukan.top == 0)
         return 1;
     else
         return 0;
 }
 
-int IsFull(){
+int IsFull() {
     if (tumpukan.top == max)
         return 1;
     else
